6175c39c6059e0a85e2f868838cff9efe0c39541
Fernando Lugo <fernando.luggo@gmail.com>
2020-04-22 19:19:07 -0500

    Proyecto RNA con colorpicker binded. Falta agregar brain.js [issue]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule, HostListener } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ColorRNA';
+  color = 'rgb(220,245,0)';
+  textColor = 'black';
+  toggle = false;
+
+  constructor() { }
+
+  ngOnInit( ) {
+
+  }
+
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
